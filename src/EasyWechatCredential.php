d876dca419b156f674c8748c771789ed3a1dffc4
osi <yaoiluo@gmail.com>
2019-07-17 18:00:10 +0800

    :memo: 增加了一行说明 :green_heart: StyleCI

--- src/EasyWechatCredential.php
+++ src/EasyWechatCredential.php
@@ -17,7 +17,6 @@ class EasyWechatCredential implements AccessTokenInterface
      * Bootstap.
      *
      * @author osi <yaoiluo@gmail.cn>
-     *
      */
     public function __construct()
     {
@@ -47,7 +46,7 @@ class EasyWechatCredential implements AccessTokenInterface
     {
         if (!isset($this->accessToken)) {
             $app = app('wechat.official_account');
-            return $app->access_token->getToken()["access_token"];
+            return $app->access_token->getToken()['access_token'];
         }
 
         return $this->accessToken;
